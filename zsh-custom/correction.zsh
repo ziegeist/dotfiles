a786bd138ac2d296104ae91db87cb9419069ea98
ziegeist <marc.chang@gmail.com>
2014-11-26 23:19:32 +0800

    first commit

---
+++ zsh-custom/correction.zsh
@@ -0,0 +1,8 @@
+setopt correct_all
+
+alias gem='nocorrect gem'
+alias man='nocorrect man'
+alias mv='nocorrect mv'
+alias mysql='nocorrect mysql'
+alias mkdir='nocorrect mkdir'
+alias rbenv='nocorrect rbenv'
